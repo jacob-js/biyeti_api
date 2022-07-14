73d05f806ce4ec4cbf7f314c2d2012909fbc0bf7
Jacob Dev <mercihabam@gmail.com>
2022-07-14 09:52:37 +0200

    fix asgi issue

--- apps/notifications/consumers.py
+++ apps/notifications/consumers.py
@@ -1,8 +1,5 @@
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
 
-from apps.notifications.models import Notification
-from apps.notifications.serializers import NotificationSerializer
-
 class UserNotificationConsumer(AsyncJsonWebsocketConsumer):
     """
     This consumer is used to send a request to the Ntification API.
@@ -15,10 +12,7 @@ class UserNotificationConsumer(AsyncJsonWebsocketConsumer):
         await self.accept()
         print("Connected to Nt")
         user_id = self.scope['url_route']['kwargs']['user_id']
-        notifications = Notification.objects.filter(user_receiver__id=user_id)
-        serializer = NotificationSerializer(notifications, many=True)
         self.channel_layer.groud_ad(user_id, self.channel_name)
-        self.send_json(serializer.data)
 
     async def disconnect(self, _):
         """
