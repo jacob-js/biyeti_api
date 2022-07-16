a0e73c2105f090102e775adfc340a2a2386ee3aa
Jacob Dev <mercihabam@gmail.com>
2022-07-16 17:03:47 +0200

    fix notification detail url issue

--- apps/notifications/urls.py
+++ apps/notifications/urls.py
@@ -4,5 +4,5 @@ from .views import get_user_notifications, notification_detail
 
 urlpatterns = [
     path('', get_user_notifications),
-    path('<int:notif_id>/', notification_detail)
+    path('<int:notif_id>', notification_detail)
 ]
